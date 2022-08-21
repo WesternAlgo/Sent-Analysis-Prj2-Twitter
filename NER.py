f9c023c03ca361234ab28e6739b96b24836db3ec
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-08-20 22:29:03 -0400

    Getting all found entities

--- NER.py
+++ NER.py
@@ -1,10 +1,10 @@
 import spacy
+import csv 
  
 nlp = spacy.load("en_core_web_sm")
- 
-text = "Sundar Pichai, the CEO of Google Inc. is walking in the streets of California."
- 
-doc = nlp(text)
- 
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+with open('tweetsList.txt','rt+') as f:
+    for line in f.readlines():
+        #lines = f.readline()
+        doc=nlp(line)
+        for ent in doc.ents:
+            print(ent.text, ent.label_)
