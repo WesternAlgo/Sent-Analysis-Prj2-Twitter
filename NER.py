961a64e97672eb84831869db1e477ca2a1118f04
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-09-11 10:17:00 -0400

    Sorted through the Entities

--- NER.py
+++ NER.py
@@ -13,4 +13,3 @@ with open('tweetsList.txt','rt+') as f:
            print(ent.text, ent.label_)
             #textF.write(ent.text, ent.label_)   
     entities = {key: list(set(map(lambda x: str(x), g))) for key, g in groupby(sorted(doc.ents, key=lambda x: x.label_), lambda x: x.label_)}
-    print(entities['DATE'])
